0965b7e26557fd06cf9e7a4e9486b01d0ef4fad5
Florian Brenot <Ziink4@users.noreply.github.com>
2021-10-13 01:24:30 +0200

    Add empty ppm files

---
+++ Core/Inc/ppm.h
@@ -0,0 +1,13 @@
+/*
+ * ppm.h
+ *
+ *  Created on: Oct 12, 2021
+ *      Author: Florian
+ */
+
+#ifndef INC_PPM_H_
+#define INC_PPM_H_
+
+
+
+#endif /* INC_PPM_H_ */
---
+++ Core/Src/ppm.c
@@ -0,0 +1,8 @@
+/*
+ * ppm.c
+ *
+ *  Created on: Oct 12, 2021
+ *      Author: Florian
+ */
+
+
