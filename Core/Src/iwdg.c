cef9566a6acc8857ffda696901c7593360e7154d
Florian Brenot <Ziink4@users.noreply.github.com>
2021-10-13 02:40:47 +0200

    Update STM32 libs

--- Core/Src/iwdg.c
+++ Core/Src/iwdg.c
@@ -28,6 +28,13 @@
 void MX_IWDG_Init(void)
 {
 
+  /* USER CODE BEGIN IWDG_Init 0 */
+
+  /* USER CODE END IWDG_Init 0 */
+
+  /* USER CODE BEGIN IWDG_Init 1 */
+
+  /* USER CODE END IWDG_Init 1 */
   LL_IWDG_Enable(IWDG);
   LL_IWDG_EnableWriteAccess(IWDG);
   LL_IWDG_SetPrescaler(IWDG, LL_IWDG_PRESCALER_8);
@@ -37,6 +44,9 @@ void MX_IWDG_Init(void)
   }
 
   LL_IWDG_ReloadCounter(IWDG);
+  /* USER CODE BEGIN IWDG_Init 2 */
+
+  /* USER CODE END IWDG_Init 2 */
 
 }
 
