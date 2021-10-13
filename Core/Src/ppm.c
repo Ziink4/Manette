4dc156edb261b1a650eea985f3d42ccd9cee23f9
Florian Brenot <Ziink4@users.noreply.github.com>
2021-10-13 13:39:29 +0200

    Change pulse duration setting

--- Core/Src/ppm.c
+++ Core/Src/ppm.c
@@ -15,6 +15,10 @@ volatile int CURRENT_CHANNEL = -1;
 volatile bool PPM_NEW_DATA;
 volatile unsigned int TIME_OLD;
 
+// Duration of pulse when an input is at minimum position
+// May vary depending on the controller
+const float PULSE_DURATION = 0.8;
+
 uint32_t MINIMAL_INTERVAL = 0;
 uint32_t MAXIMAL_INTERVAL = 0;
 uint32_t TIMEOUT_INTERVAL = 0;
@@ -26,9 +30,9 @@ void PPM_Init(uint32_t timer_freq)
   PPM_NEW_DATA = 0;
   CURRENT_CHANNEL = -1;
 
-  MINIMAL_INTERVAL = (1 * timer_freq / 1000); // 1ms = Stick min position
-  MAXIMAL_INTERVAL = (2 * timer_freq / 1000); // 2ms = Stick max position
-  TIMEOUT_INTERVAL = (3 * timer_freq / 1000); // 3ms = Timeout
+  MINIMAL_INTERVAL = (1 * PULSE_DURATION * timer_freq / 1000); // Stick min position
+  MAXIMAL_INTERVAL = (2 * PULSE_DURATION * timer_freq / 1000); // Stick max position
+  TIMEOUT_INTERVAL = (3 * PULSE_DURATION * timer_freq / 1000); // Timeout
 }
 
 unsigned char PPM_GetChannel(unsigned int channel)
@@ -59,6 +63,7 @@ void PPM_TimeoutHandler()
 uint32_t PPM_PulseHandler(uint32_t pulse_width)
 {
   uint32_t time = pulse_width;
+
   if (time > TIME_OLD)
   {
     time = time - TIME_OLD;
