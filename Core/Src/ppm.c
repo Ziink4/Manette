4a04707bc42dc4bd09d768e5b437cf071667f757
Florian Brenot <Ziink4@users.noreply.github.com>
2021-10-13 13:42:30 +0200

    Use uint32_t for PPM data

--- Core/Src/ppm.c
+++ Core/Src/ppm.c
@@ -8,12 +8,11 @@
 #include "ppm.h"
 
 #define NUM_CHANNELS 8
-volatile unsigned int ppm[NUM_CHANNELS];
+volatile uint32_t ppm[NUM_CHANNELS];
 
 volatile int CURRENT_CHANNEL = -1;
-
 volatile bool PPM_NEW_DATA;
-volatile unsigned int TIME_OLD;
+volatile uint32_t TIME_OLD;
 
 // Duration of pulse when an input is at minimum position
 // May vary depending on the controller
@@ -37,13 +36,13 @@ void PPM_Init(uint32_t timer_freq)
 
 unsigned char PPM_GetChannel(unsigned int channel)
 {
-  unsigned int t = ppm[channel];
+  uint32_t t = ppm[channel];
 
   // Convert MINIMAL_INTERVAL...MAXIMAL_INTERVAL -> 0...255
   if (t < MINIMAL_INTERVAL) t = MINIMAL_INTERVAL;
 
   t -= MINIMAL_INTERVAL;
-  t /= ((MAXIMAL_INTERVAL - MINIMAL_INTERVAL) / 255);
+  t /= (MAXIMAL_INTERVAL - MINIMAL_INTERVAL) / 255;
 
   if (t > 255) t = 255;
 
